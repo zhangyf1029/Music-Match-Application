6c324dc64912dd43fae3a5e9b1069f0aebd66dab
susri523 <57196453+susri523@users.noreply.github.com>
2021-04-09 13:45:40 -0400

    Oauth to get spotify top reads

--- dating-app/backend/app.py
+++ dating-app/backend/app.py
@@ -1,17 +1,45 @@
 import time 
 from flask import Flask
 from flask import render_template, request
+from startup import *
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return {'text': "Hello World!"}
+def oauth_spotify():
+    data =  getUser()
+    return render_template('oauth_callback.html', data=data)
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/callback/')
+def callback():
+    code = request.args.get('code', default = '', type=str)
+    getUserToken(code)
+    token = getAccessToken()
+    auth_header = token[1]['Authorization']
+    headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': auth_header,
+}
+    response = requests.get('https://api.spotify.com/v1/me/top/artists', headers=headers)
+    data = response.json()
+    return render_template('callback_return.html', data=data)
+
+
+
+
+
+
+
+        
+
+
 
 if __name__ == '__main__':
     app.run()
---
+++ dating-app/backend/startup.py
@@ -0,0 +1,31 @@
+from flask_spotify_auth import getAuth, refreshAuth, getToken
+
+#Add your client ID
+CLIENT_ID = "7c1f2bab60504b6e91baaf93b7ff0c10"
+
+#aDD YOUR CLIENT SECRET FROM SPOTIFY
+CLIENT_SECRET = "c27bc4507c8b41a6b3ffed7db05c21d6"
+
+#Port and callback url can be changed or ledt to localhost:5000
+PORT = "5000"
+CALLBACK_URL = "http://localhost"
+
+#Add needed scope from spotify user
+SCOPE = "user-top-read" #"streaming user-read-birthdate user-read-email user-read-private"
+#token_data will hold authentication header with access code, the allowed scopes, and the refresh countdown 
+TOKEN_DATA = []
+
+
+def getUser():
+    return getAuth(CLIENT_ID, "{}:{}/callback/".format(CALLBACK_URL, PORT), SCOPE)
+
+def getUserToken(code):
+    global TOKEN_DATA
+    TOKEN_DATA = getToken(code, CLIENT_ID, CLIENT_SECRET, "{}:{}/callback/".format(CALLBACK_URL, PORT))
+ 
+def refreshToken(time):
+    time.sleep(time)
+    TOKEN_DATA = refreshAuth()
+
+def getAccessToken():
+    return TOKEN_DATA
