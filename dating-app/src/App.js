6b5a289a2934b2f64d914ba5d3e46d71453e97cf
susri523 <57196453+susri523@users.noreply.github.com>
2021-04-11 16:59:55 -0400

    Sign up form with react

--- dating-app/src/App.js
+++ dating-app/src/App.js
@@ -1,23 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react'; //, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const [currentTime, setCurrentTime] = useState(0);
 
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
+class SignUpForm extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {firstName: null, pronouns: null, preferences: null};
 
-  return (
-    <div className="App">
-      <header className="App-header">
-  <p>The current time is {currentTime}.</p>
-</header>
-</div>
-  );
+      this.handleChange = this.handleChange.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+
+    render(){
+      return (
+        <form>
+          <label> First name: 
+          <input name="firstName" type="text" value={this.state.firstName} onChange={this.handleChange} />
+          </label>
+          <br />
+
+          <label> Pronouns: 
+          <input name="pronouns" type="text" value={this.state.pronouns} onChange={this.handleChange} />
+          </label>
+          <br />
+
+          <label> Preferences: 
+          <input name="preferences" type="text" value={this.state.preferences} onChange={this.handleChange} />
+          </label>
+          <br />
+
+        </form>
+      );
+    }
 }
 
-export default App;
+
+export default SignUpForm;
