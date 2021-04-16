739dd8394c148ae6fa88e55509984c74f982bc3d
susri523 <57196453+susri523@users.noreply.github.com>
2021-04-16 13:19:12 -0400

    tried new code for init

--- dating-app/backend/__init__.py
+++ dating-app/backend/__init__.py
@@ -6,6 +6,10 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+#migrate = Migrate(app, db)
+
+migrate = Migrate()
+migrate.init_app(app, db)
+
 
 #from .app import models #removed routes
