24281734ed43a2c7547756d25c8110bb9cd9d1ec
susri523 <57196453+susri523@users.noreply.github.com>
2021-04-24 19:58:18 -0400

    login working and signup working but not current_user.is_auth

--- dating-app/backend/models.py
+++ dating-app/backend/models.py
@@ -2,6 +2,18 @@ from . import db
 
 #https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-your-app-with-flask-login 
 
+class UserProfile(db.Model):
+    email = db.Column(db.String(120), unique=True, primary_key=True)
+    first_name = db.Column(db.String(64), index=True)
+    last_name = db.Column(db.String(64), index=True)
+    pronouns = db.Column(db.String(64), index=True)
+    preferences =  db.Column(db.String(64), index=True)
+    dob = db.Column(db.Date())
+    token = db.Column(db.String(300))
+    password = db.Column(db.String(100))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.first_name) 
 
 class DatingProfile(db.Model):
     email = db.Column(db.String(120), unique=True, primary_key=True)
