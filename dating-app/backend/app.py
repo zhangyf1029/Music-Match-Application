cd224e7c274ce459d0f3ffdca709c1246abd6e87
susri523 <57196453+susri523@users.noreply.github.com>
2021-04-25 17:39:38 -0400

    working w/Swastik genreConsolidator, and I got the second api call working

--- dating-app/backend/app.py
+++ dating-app/backend/app.py
@@ -7,6 +7,9 @@ from .flask_spotify_auth import getRefreshToken, refreshAuth
 import requests
 import sqlite3
 
+import itertools, operator 
+from collections import Counter
+
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from .db import get_db
@@ -127,7 +130,6 @@ def signup():
 
         # create a new user with the form data. Hash the password so the plaintext version isn't saved.
         db.execute("INSERT INTO user_profile VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);", (email, first_name, last_name, pronouns, preferences, dob, token, refresh_token, generate_password_hash(password, method='sha256')) )
-        conn.commit()
 
         # add the new user to the database
         # db.session.add(new_user)
@@ -268,7 +270,7 @@ def getUserTopArtist():
             url.append(artist['images'][0]['url'])
     conn.close()
 
-    return render_template('top_artist.html', name=name, url=url)
+    return render_template('top_artist.html', name=name, url=url, len=len(name))
 
 @app.route('/getTopArtist', methods=['POST'])
 def getTopArtist(email):
@@ -331,12 +333,8 @@ def getMatchPercent(self_email, other_email):
          #other[1] is names of top artists
          if self_top_artist in other_top[1]:
              match_count+= 1
+    # return (match_count / len(self_top[1])) * 100
     return (match_count / 20) * 100
-
-
-
-
-
 
 @app.route('/getMatches', methods=['GET', 'POST'])
 def getMatches():
@@ -348,10 +346,158 @@ def getMatches():
             other_email = user['email']
             user_top = getTopArtist(other_email)  #returns [users, name, url]
             match_percent = getMatchPercent(self_email, other_email)
-            user_top.append(match_percent)
-            all_users_top.append(user_top)
-    
-        return render_template("all_user.html", all_users=all_users_top)
+            if(match_percent > 0):
+                user_top.insert(0, match_percent)
+                all_users_top.append(user_top)
+        if all_users_top:
+            all_users_top.sort(reverse=True) #[ match_percent, users, name_top_artists, url_top_artist]
+        size = len(all_users_top)
+        return render_template("all_user.html", self_email=self_email, all_users=all_users_top, size = size)
+
+@app.route('/getTopGenres', methods=['POST'])
+def getTopGenres(email):
+    conn = sqlite3.connect('db.sqlite')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM user_profile where email='{0}'".format(email))
+    users = cursor.fetchone()
+
+    if users:
+        token = users[6]
+        refresh_token = users[7]
+
+        authorization = f'Bearer {token}'      
+
+        headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': authorization,
+        }
+
+        response = requests.get('https://api.spotify.com/v1/me/top/artists', headers=headers)
+        data = response.json()
+        if 'error' in data.keys():
+            new_token_info = refreshAuth(refresh_token)
+            new_access_token = new_token_info['access_token']
+            cursor.execute("UPDATE user_profile set access_token = '{0}' where email='{0}'".format(new_access_token, email))
+            conn.commit()
+            cursor.execute("SELECT * FROM user_profile where email='{0}'".format(email))
+            users = cursor.fetchone()
+
+            authorization = f'Bearer {new_access_token}'      
+
+            headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': authorization,
+            }
+
+            response = requests.get('https://api.spotify.com/v1/me/top/artists', headers=headers)
+            data = response.json()
+
+        data = data['items']
+        genres = []
+
+        for artist in data:
+            genres.append(artist['genres'])
+            
+        conn.close()
+
+        return [users, genreListConsolidate(genres)]
+
+def genreCategorize(genre):
+    if('pop' in genre):
+        genre = "Pop"
+    elif('rap' in genre):
+        genre = "Hip-hop/Rap"
+    elif('hip-hop' in genre):
+        genre = "Hip-hop/Rap"
+    elif('trap' in genre):
+        genre = "Hip-hop/Rap"
+    elif('drill' in genre):
+        genre = "Hip-hop/Rap"
+    elif('house' in genre):
+        genre = "Dance/Electronic"
+    elif('country' in genre):
+        genre = "Country"
+    elif("classical" in genre):
+        genre = "Classical"
+    elif('alt' in genre):
+        genre = "Altenative"
+    elif("indie" in genre):
+        genre = "Alternative"
+    elif("edm" in genre):
+        genre = "Dance/Electronic"
+    elif("dance" in genre):
+        genre = "Dance/Electronic"
+    elif("electronic" in genre):
+        genre = "Dance/Electronic"
+    elif("electric" in genre):
+        genre = "Dance/Electronic"
+    elif("dubstep" in genre):
+        genre = "Dance/Electronic"
+    elif('jazz' in genre):
+        genre = "Jazz"
+    elif("latin" in genre):
+        genre = "Latin"
+    elif("spanish" in genre):
+        genre = "Latin"
+    elif('metal' in genre):
+        genre = "Metal"
+    elif("rock" in genre):
+        genre = "rock"
+    elif("punk" in genre):
+        genre = "rock"    
+    elif("R&B" in genre):
+        genre = "R&B"    
+    elif("blues" in genre):
+        genre = "R&B" 
+    else:
+        genre = "Other"
+
+    return genre
+
+
+def genreListConsolidate(genre_list):
+    flat_list = list(itertools.chain(*genre_list))
+
+    genres = {x:flat_list.count(x) for x in flat_list}
+
+    genres_umbrella = [genreCategorize(spotify_genre) for spotify_genre in flat_list]
+
+    genres = {x:genres_umbrella.count(x) for x in genres_umbrella}
+
+    sorted_genres = dict(sorted(genres.items(), key=operator.itemgetter(1),reverse=True))
+
+    genre_keys = list(sorted_genres.keys())
+
+    return genre_keys
+
+def compareGenres(self_genre, other_genre):
+    both_genre = [genre for genre in self_genre if genre in other_genre]
+    return both_genre
+
+
+@app.route('/getEvents', methods=['GET', 'POST'])
+def getEvents():
+    if request.method == 'POST':
+        self_email = request.form.get('self_email')
+        other_email = request.form.get('other_email')
+
+        user_top = getTopGenres(self_email) # [users, genres] where genres is a sorted list
+        other_top = getTopGenres(other_email) # [users, genres]
+
+        #compare genres
+        genre = compareGenres(user_top[1], other_top[1])
+
+        # return render_template("match_events.html", data_self=user_top, data_other=other_top)
+        
+        classificationName = genre[0] #ENTER GENRE NAME
+        response = requests.get(f"https://app.ticketmaster.com/discovery/v2/events.json?apikey=PBSmqVGp0ZUUCVC3VKJ3oTH3SWnidD7S&classificationName=music&countryCode=US&postalCode=02215&classificationName={classificationName}")
+        json_res = response.json()
+        if json_res["page"]["totalElements"] == 0:
+            return render_template('no_results.html')
+        else: 
+            return render_template("match_events.html", events=list(json_res["_embedded"]["events"]))
 
     
 if __name__ == '__main__':
